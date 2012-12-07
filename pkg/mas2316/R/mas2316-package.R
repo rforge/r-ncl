bd8d2a57f44a3753eae863edff8daf1a6587ef0c
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-07 17:13:24 +0000

    initial commit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mas2316/R/mas2316-package.R
@@ -0,0 +1,12 @@
+#' MAS1343
+#' 
+#' Functions and datasets used in MAS1343: Introduction to 
+#' statistical computing with R
+#' @name mas1343-package
+#' @title MAS1343
+#' @aliases mas1343 MAS1343
+#' @docType package
+#' @author \email{colin.gillespie@@newcastle.ac.uk}
+#' @keywords package
+#' @seealso See the Blackboard homepage for further details.
+NULL

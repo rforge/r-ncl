660c6f1415ee0db010faf1973648c9509c326bb4
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-07 17:17:39 +0000

    Updating description and package details
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mas2316/R/mas2316-package.R
+++ pkg/mas2316/R/mas2316-package.R
@@ -1,10 +1,9 @@
-#' MAS1343
+#' MAS2316
 #' 
-#' Functions and datasets used in MAS1343: Introduction to 
-#' statistical computing with R
-#' @name mas1343-package
-#' @title MAS1343
-#' @aliases mas1343 MAS1343
+#' Functions and datasets used in MAS2316: Introduction to regression and modelling
+#' @name mas2316-package
+#' @title MAS2316
+#' @aliases mas2316 MAS2316
 #' @docType package
 #' @author \email{colin.gillespie@@newcastle.ac.uk}
 #' @keywords package

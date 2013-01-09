28311817a0d522e487e540911e76b44f36770727
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-09 16:27:24 +0000

    Added the bodyweigth data set
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mas2316/R/bodyweight.R
@@ -0,0 +1,13 @@
+#' The bodyweight data set
+#' 
+#' The average weight of the brain and body for sixty-two mammal species were
+#' recorded. We wish to predict the weight of the body using the brain weight. 
+#' The first column is the subject index, the second is log of the brain weight 
+#' and third is the log of the body weight.
+#' @name bodyweight
+#' @docType data
+#' @return A data frame
+#' @keywords datasets
+#' @examples
+#' data(bodyweight)
+NULL

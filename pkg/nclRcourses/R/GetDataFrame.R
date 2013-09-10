62827ed588b8d3fed9b2ea079002150edf61b4f4
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-10 16:28:50 +0000

    First version of package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRcourses/R/GetDataFrame.R
@@ -0,0 +1,32 @@
+globalVariables("yeast_long")
+#' Subset of the yeast data set
+#' 
+#' This function creates a subset (of random rows) of the yeast data set
+#' data frame. The seed argument resets the random number generator.
+#' @param seed a positive interger for the random number generator. Default 1.
+#' @return A data frame with a random numer of rows
+#' @export
+#' @examples
+#' ##Given the same student id
+#' ##the same data set is returned
+#' d1 = GetDataFrame() 
+#' d2 = GetDataFrame(10) 
+#' identical(d1, d2)
+GetDataFrame = function(seed=1) {
+  set.seed(seed)
+  data(yeast_long)
+  d = yeast_long
+  l = sample(50:100, 1)
+  del_rows = sample(1:nrow(d), l)
+  
+  d = d[-del_rows,]
+  rownames(d) = 1:nrow(d)
+
+  return(d)
+}
+
+
+
+
+
+
---
+++ pkg/nclRcourses/R/nclRcourses-package.R
@@ -0,0 +1,9 @@
+#' R courses at Newcastle University
+#' 
+#' Functions and datasets used in for external R courses at Newcastle University
+#' @name nclRcourses-package
+#' @aliases nclRcourses nclrcourses
+#' @docType package
+#' @author \email{colin.gillespie@@newcastle.ac.uk}
+#' @keywords package
+NULL

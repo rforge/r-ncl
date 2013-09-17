3a4b07d8260f21178d96877967ddf1db105a6c1d
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-17 15:26:34 +0000

    Added normalised data
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@27 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRcourses/R/raw_data.R
@@ -0,0 +1,13 @@
+#' Raw and post processed data
+#' 
+#' This data comes from Cockell et al., 2013.
+#' \url{https://github.com/csgillespie/illumina-analysis/}
+#' @aliases analysis_ready_data raw_data
+#' @name yeast_long
+#' @docType data
+#' @usage data(yeast_long)
+#' @references Cockell, S, Bashton, M, Gillespie, CS. 
+#' Bioconductor tools for microarray data analysis. 
+#' In: Microarray Image and Data Analysis: Theory and Practice. 
+#' CRC Press.
+NULL

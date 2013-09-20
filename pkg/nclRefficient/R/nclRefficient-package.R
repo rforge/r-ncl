f90ae050cd4b2cee108587eecefe9c07186c4202
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-20 15:38:20 +0000

    First version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRefficient/R/nclRefficient-package.R
@@ -0,0 +1,7 @@
+#' R courses at Newcastle University
+#' 
+#' Functions and datasets used in for external R courses at Newcastle University
+#' @name nclRefficient-package
+#' @aliases nclRefficient
+#' @docType package
+NULL

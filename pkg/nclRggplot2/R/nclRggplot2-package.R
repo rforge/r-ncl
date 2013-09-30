900dcdb9e0767ff2862eb689087a82b93d0a7493
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-30 14:57:38 +0000

    First version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRggplot2/R/nclRggplot2-package.R
@@ -0,0 +1,7 @@
+#' R courses at Newcastle University
+#' 
+#' Functions and datasets used in for external R courses at Newcastle University
+#' @name nclRggplot2-package
+#' @aliases nclRggplot2
+#' @docType package
+NULL

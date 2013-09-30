3431ccc6a3729ea923466a5cb64f451d32668b21
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-30 20:48:31 +0000

    Adding (placeholder) man data pages
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nclRggplot2/R/datasets.R
+++ pkg/nclRggplot2/R/datasets.R
@@ -1,29 +1,39 @@
-#' Movie information and user ratings from IMDB.com.
+#' Beauty data set
 #' 
-#' The internet movie database, \url{http://imdb.com/}, is a 
-#' website devoted to collecting movie data supplied by studios and fans.
-#' It claims to be the biggest movie database on the web and is run by 
-#' amazon.  More about information imdb.com can be found online,
-#' \url{http://imdb.com/help/show_leaf?about}, including 
-#' information about the data collection process,
-#' \url{http://imdb.com/help/show_leaf?infosource}.
-#' @name movies
+#' The beauty data set
+#' @name Beauty
 #' @docType data
-#' @usage data(movies)
-#' @return A data frame with 4848 rows and 24 variables.
-#' @note Movies were selected for inclusion if they had a known length, had been rated by at least one imdb user and had an mpaa rating. The  data set contains the following fields:
-#' \describe{
-#' \item{title.}{Title of the movie.}
-#' \item{year.}{Year of release.}
-#' \item{budget.}{Total budget (if known) in US dollars}
-#' \item{length.}{Length in minutes.}
-#' \item{rating.}{Average IMDB user rating.}
-#' \item{votes.}{Number of IMDB users who rated this movie.}
-#' \item{r1-10.}{Multiplying by ten gives percentile (to nearest 10\%) of users who rated this movie a 1.}
-#' \item{mpaa.}{MPAA rating.}
-#' \item{action, animation, comedy, drama, documentary, romance, short.}{Binary variables representing if movie was classified as belonging to that genre.}}
-#'@references Credit: This data set was initially constructed by 
-#' Hadley Wickham at \url{http://had.co.nz/data/movies}
-#' @aliases Budget Length movies
+#' @usage data(Beauty)
+#' @return A data frame
+#'@keywords datasets
+NULL
+
+#' Aphids data set
+#' 
+#' The aphids data set
+#' @name aphids
+#' @docType data
+#' @usage data(aphids)
+#' @return A data frame
+#'@keywords datasets
+NULL
+
+#' Google data set
+#' 
+#' The google data set
+#' @name google
+#' @docType data
+#' @usage data(google)
+#' @return A data frame
+#'@keywords datasets
+NULL
+
+#' Treats data set
+#' 
+#' The treats data set
+#' @name treats
+#' @docType data
+#' @usage data(treats)
+#' @return A data frame
 #'@keywords datasets
 NULL

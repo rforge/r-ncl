62cde36ee6f6ed17ee80dd09761633f63fadbfd4
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-23 14:55:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@78 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRprogrammingB/R/datasets.R
@@ -0,0 +1,16 @@
+#' Dummy data set
+#' 
+#' This data frame dummy_data represents an experiment, 
+#' where we have ten treatments:
+#' A, B, ..., J and measurements at some time points. 
+#' We want to create a scatter plot of measurement against time, 
+#' for each treatment type.
+#' @name dummy_data
+#' @aliases dummy_data
+#' @docType data
+#' @usage data(dummy_data)
+#' @return A data frame 
+#' @keywords datasets
+#' @examples
+#' data(dummy_data)
+NULL
---
+++ pkg/nclRprogrammingB/R/nclRprogramming-package.R
@@ -0,0 +1,8 @@
+#' R courses at Newcastle University
+#' 
+#' Functions and datasets used in for external R courses at Newcastle University
+#' @name nclRprogramming-package
+#' @aliases nclRprogramming
+#' @docType package
+#' @keywords package
+NULL

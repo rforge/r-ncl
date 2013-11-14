1bc24e03d34b557937797f3c290ae0c948b6b62e
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-14 17:33:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@87 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRadvanced/R/nclRadvanced-package.R
@@ -0,0 +1,10 @@
+#' R courses at Newcastle University
+#' 
+#' Functions and datasets used in for R 
+#' courses at Newcastle University. This package is used in
+#' the "Advanced R programming" course".
+#' @name nclRadvanced-package
+#' @aliases nclRadvanced nclradvanced
+#' @docType package
+#' @keywords package
+NULL

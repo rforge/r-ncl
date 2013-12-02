5c56bf9d445489061b788f559c48e5d54889d24d
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-02 10:52:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nclRprogrammingB/R/nclRprogramming-package.R
+++
@@ -1,8 +0,0 @@
-#' R courses at Newcastle University
-#' 
-#' Functions and datasets used in for external R courses at Newcastle University
-#' @name nclRprogramming-package
-#' @aliases nclRprogramming
-#' @docType package
-#' @keywords package
-NULL

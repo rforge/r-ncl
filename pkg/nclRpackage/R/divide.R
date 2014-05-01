4de9cca49b4de11a64614133361bcec90dac2894
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-01 11:02:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@136 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRpackage/R/divide.R
@@ -0,0 +1,7 @@
+#' A function for dividing
+#' 
+#' A very useful function, that is excellent at dividing numbers
+#' @param x a number
+#' @param y another number
+#' @export
+div = function(x, y) x/y

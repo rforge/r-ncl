b40534db295877e75d5be38ddd406e943672eb29
csgillespie <csgillespie@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-01 19:47:02 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@137 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/nclRadvanced/R/get_args.R
+++ pkg/nclRadvanced/R/get_args.R
@@ -1,8 +1,11 @@
+#' Print the ... argument
+#' 
+#' A simple function that explores what happens when use ...
+#' @export
 print_dots = function(...) {
     dots_list = eval(substitute(alist(...)))
-    dots_vec = unlist(g)
+    dots_vec = unlist(dots_list)
     for(n in names(dots_vec))
       message(n, " = ", dots_vec[n])
     invisible(dots_vec)
 }
-

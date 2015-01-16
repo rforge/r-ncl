d27e4333b3ea1d1036e4220a0526165f3110f9df
sarahheaps <sarahheaps@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-16 16:33:44 +0000

    Added footie data frame.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-ncl@160 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nclRintroduction/R/footie.R
@@ -0,0 +1,23 @@
+#' Results from matches in the Premier League
+#' 
+#' Results from matches in the English Premier League 
+#' from the 2007/08 season to the 2010/11 season
+#' 
+#' The Premier League is an English professional league for 
+#' men's association football clubs.
+#' @name footie
+#' @docType data
+#' @usage data(footie)
+#' @return A data frame with 1520 rows and 6 variables.
+#' @note The  data set contains the following fields:
+#' \describe{
+#' \item{Date.}{Date of match.}
+#' \item{HomeTeam.}{Name of the home team.}
+#' \item{AwayTeam.}{Name of the away team.}
+#' \item{FTR.}{Full time result; a factor with levels A, D, H representing an away win, draw or home win.}
+#' \item{HY.}{Number of home team yellow cards.}
+#' \item{AY.}{Number of away team yellow cards.}}
+#'@keywords datasets
+#' @examples
+#' data(footie)
+NULL
